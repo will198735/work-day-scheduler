17816617568ef1a5ad5bc93fef19cdbb3c21fe32
will198735 <recinoswilfredo@gmail.com>
2022-12-19 20:55:17 -0800

    modified HTML add deloped and assets

--- Develop/assets/js/script.js
+++ Develop/assets/js/script.js
@@ -1,7 +1,30 @@
-// Wrap all code that interacts with the DOM in a call to jQuery to ensure that
-// the code isn't run until the browser has finished rendering all the elements
-// in the html.
+var timeDisplayEL = $('.time-display');
+
+
+function displayTime() {
+  var rightNow = dayjs().format('MMM DD, YYYY [at] hh:mm:ss a');
+  timeDisplayEL.text(rightNow);
+}
+
+
+
 $(function () {
+
+displayTime();
+setInterval(displayTime, 1000);
+
+
+
+
+
+
+
+
+
+
+  // Wrap all code that interacts with the DOM in a call to jQuery to ensure that
+  // the code isn't run until the browser has finished rendering all the elements
+  // in the html.
   // TODO: Add a listener for click events on the save button. This code should
   // use the id in the containing time-block as a key to save the user input in
   // local storage. HINT: What does `this` reference in the click listener
